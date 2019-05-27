0ee2315d80f315d807d083ad506ce79a8dc709e3
Andrew Bond <git.bond@xoxy.net>
2019-05-27 12:20:29 +0700

    Fix: Respect bool to add priv remote to pub repo
    Docs: Improve documentation
    
    Fix: Properly respect the boolean variable on whether to add a private
    remote to the clone of the public repository.

--- fork-public-repo-to-private/README.md
+++ fork-public-repo-to-private/README.md
@@ -1,9 +1,13 @@
 # Make a private fork of a public git repository ![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightscreen.svg)
 
+Including ability to sync code between the public repo and the fork.
+
 ## Getting started
 
 Edit the variables at the top of the shell script and execute it!
 
-### Based on
+<!---## Description of basic process
+Create a---> 
+### Inspired by
 
-Based on the post *[Make a private fork of a public git repository](https://steinbaugh.com/posts/git-private-fork.html)*.
+Inspired by the post *[Make a private fork of a public git repository](https://steinbaugh.com/posts/git-private-fork.html)*.
--- fork-public-repo-to-private/fork-public-repo-to-private.sh
+++ fork-public-repo-to-private/fork-public-repo-to-private.sh
@@ -1,67 +1,69 @@
 #!/usr/bin/env bash
-set -o errexit # Set the shell option to exit immediately if a command exits with a non-zero status.
-set -o verbose # Set the shell option to print shell input lines as they are read (but without variable expansion/substitution)
+
+# Set the shell option to exit immediately if a command exits with a non-zero status.
+set -o errexit
+
+# Set the shell option to print shell input lines as they are read (but without variable expansion/substitution)
+set -o verbose
 
 # If you have just downloaded this script you may need to set it to executable
 : '
 chmod +x fork-public-repo-to-private.sh
 '
 
-# Variables to set - https://github.com/${public_user}/${public_repo}"
+# First, create a corresponding private repo in your user account:
+# https://github.com/new
+
+# Variables to set - example: https://github.com/andrewdbond/shell-scripts
 public_user="andrewdbond"
 public_repo="shell-scripts"
-
 private_user="andrewdbond"
 private_repo="shell-scripts"
 
 keep_public_repo_clone=false
 add_private_remote_to_public_repo=false
 
-# Can use git@github.com
+# Can use SSH:
 git_url="git@github.com:"
 # Or if HTTPS is needed instead:
 # git_url="https://github.com/"
 
-# If we'll be pushing commits to the public repo, we recommend creating a branch other than master
-# in the public repo that will be the destination for commit merges from the private repo. In general,
-# develop works well for this purpose.
+# If we'll be pushing commits to the public repo, we recommend creating a branch
+# other than master in the public repo that will be the destination for commit
+# merges from the private repo. In general, develop works well for this purpose.
 branch="master"
 
-# Now we’re ready to define and clone the repos from GitHub. First, create a corresponding
-# private repo in your user account using the new repo UI: https://github.com/new
-# In our example here, the name should be basejump-private.
+# Now we’re ready to proceed.
 public_repo_full_url="$git_url$public_user/$public_repo.git"
 private_repo_full_url="$git_url$private_user/$private_repo.git"
 
-# Clone (mirror) the existing public repo into our newly created private repo on GitHub. Note
-# that this won’t affect anything in the existing public one.
+# Clone (mirror) the existing public repo into our newly created private repo.
+# Note that this won’t affect anything in the existing public one.
 git clone --bare "$public_repo_full_url"
 cd "$public_repo.git"
 git push --mirror "$private_repo_full_url"
 
-# Safe to remove the bare clone.
+# Remove the bare clone.
 cd ..
 rm -rf "$public_repo.git"
 
 # Clone a local copy private repo.
 git clone "$private_repo_full_url"
 
-# Ensure we switch away from the master branch in our new private repo.
+# Switch to the desired branch in our new private repo.
 cd "$private_repo"
-
-# Switch away from master branch.
 git checkout -b "$branch"
 git branch --set-upstream-to="origin/$branch" "$branch"
+
+# In the private repo, add a remote back to the public repo
 git remote add "public" "$public_repo_full_url"
 cd ..
 
-
-# Set up remotes to the private repo in the public repo, so we can push. We’re switching to the
-# corresponding branch (e.g. “develop” here).
+# Set up remotes to the private repo in the public repo, so we can push.
 if [ "$keep_public_repo_clone" = true ] ; then
     git clone "$public_repo_full_url"
     cd "$public_repo"
-    if [ "$keep_public_repo_clone" = true ] ; then
+    if [ "$add_private_remote_to_public_repo" = true ] ; then
         git remote -v
         git remote add "$private_repo" "$private_repo_full_url"
     fi
@@ -69,12 +71,13 @@ if [ "$keep_public_repo_clone" = true ] ; then
     cd ..
 fi
 
-# Here’s how to make a pull request from the private repo back to the public one. Then simply
-# make a pull request from the branch (e.g. develop) back to master.
+# Here’s how to make a pull request from the private repo back to the public
+# one. Then simply make a pull request from the branch (e.g. develop) back to
+# master.
 #
 # cd "$public_repo"
 # git pull "$private_repo" "$branch"
 # git push origin "$branch"
 # cd ..
 
-# Based off of https://steinbaugh.com/posts/git-private-fork.html
+# Inspired by https://steinbaugh.com/posts/git-private-fork.html
